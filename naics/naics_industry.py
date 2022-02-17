7d0ca4b46ceef05aa8d0a2d5be44913f82006373
dylanmoring <dylan.a.moring@gmail.com>
2022-02-16 18:23:41 -0700

    Add NAICSIndustry.included_activities property Give most NAICSIndustry properties hidden attributes

--- naics/naics_industry.py
+++ naics/naics_industry.py
@@ -17,15 +17,40 @@ class NAICSIndustry(NAICSIndustryCode):
 
     @property
     def equivalent_codes(self):
-        return [code for code in self.naics_objects.values() if code.full_code == self.full_code and code is not self]
+        if not hasattr(self, '_equivalent_codes'):
+            self._equivalent_codes = [
+                code for code in self.naics_objects.values() if code.full_code == self.full_code and code != self
+            ]
+        return self._equivalent_codes
 
     @property
     def child_codes(self):
-        return [code for code in self.naics_objects.values() if str(code.code).startswith(str(self.code))]
+        if not hasattr(self, '_child_codes'):
+            self._child_codes = [
+                code for code in self.naics_objects.values() if str(code.code).startswith(str(self.code))
+            ]
+        return self._child_codes
+
+    @property
+    def included_activities(self):
+        if not hasattr(self, '_included_activities'):
+            if self.index_items:
+                included_activities = set(self.index_items)
+            else:
+                included_activities = set()
+            for child_code in self.child_codes:
+                if child_code.index_items:
+                    included_activities.update(child_code.index_items)
+            self._included_activities = sorted(list(included_activities))
+        return self._included_activities
 
     @property
     def parent_codes(self):
-        return [code for code in self.naics_objects.values() if str(self.code).startswith(str(code.code))]
+        if not hasattr(self, '_parent_codes'):
+            self._parent_codes = [
+                code for code in self.naics_objects.values() if str(self.code).startswith(str(code.code))
+            ]
+        return self._parent_codes
 
     @property
     def previous_code(self):
